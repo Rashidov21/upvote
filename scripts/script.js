1393f83121e5ff7ce581acf0c11830b90afa9dbb
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-02-19 09:51:07 +0500

    update design

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,11 @@
 
 function OpenCloseMenu() {
     let menu = document.querySelector(".mobile-menu");
+    let menuIcon = document.querySelector("#menu-icon");
     menu.classList.toggle("active");
+    if (menu.classList.contains("active")) {
+        menuIcon.src = './src/cross.svg'
+    } else {
+        menuIcon.src = './src/menu-hamburger.svg'
+    }
 }
