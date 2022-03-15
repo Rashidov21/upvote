01319d0cd9a49bd26c8448bccb3e95faa0435a77
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-03-15 12:52:27 +0500

    reload project files

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,27 @@
 // MOBILE MENU 
 
-function OpenCloseMenu() {
-    let menu = document.querySelector(".mobile-menu");
-    let menuIcon = document.querySelector("#menu-icon");
-    menu.classList.toggle("active");
-    if (menu.classList.contains("active")) {
-        menuIcon.src = './src/cross.svg'
-    } else {
-        menuIcon.src = './src/menu-hamburger.svg'
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
     }
-}
+
+});
